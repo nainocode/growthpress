2d5f43fb4dcc600ac346921f2a412bd062d9419c
Husnain5811 <husnainrazaghulamraza@gmail.com>
2026-05-02 21:14:33 -0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
 import { ThemeProvider } from "@/components/layout/theme-provider";
 import { siteConfig } from "@/lib/seo";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -54,8 +55,8 @@ export default function RootLayout({
           <main className="mx-auto min-h-[calc(100vh-140px)] w-full max-w-6xl px-4 py-8 sm:px-6">
             {children}
           </main>
-
           <Footer />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
